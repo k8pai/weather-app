f82c92da7c60475e95c7c3f1f8550185ea7756fe
k8pai <thek8pai@gmail.com>
2023-03-14 00:54:10 +0530

    updated with functions and state modifications

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsClouds } from 'react-icons/bs';
 import SimpleContent from '../components/SimpleContent';
 import WeatherCard from '../components/WeatherCard';
 
 export default function Home() {
-	const [address, setAddress] = useState('');
-	const [subregion, setSubregion] = useState('');
-	const [state, setState] = useState('');
-	const [country, setCountry] = useState('');
-	const latitude = 12.966180000000065;
-	const longitude = 77.58690000000007;
+	const [location, setLocation] = useState({
+		country: '',
+		state: '',
+		subregion: '',
+		latitude: '',
+		longitude: '',
+	});
 	const [data, setData] = useState([]);
 
-	// const placeOptions = {
-	// 	method: 'GET',
-	// 	headers: {
-	// 		'X-RapidAPI-Key':
-	// 			'74dcbb2682msha0be34e87a8094cp16a5dejsn094540852091',
-	// 		'X-RapidAPI-Host':
-	// 			'address-from-to-latitude-longitude.p.rapidapi.com',
-	// 	},
-	// };
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
 
-	// fetch(
-	// 	`https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi?address=${address}%20${subregion}%20${state}%20${country}`,
-	// 	placeOptions,
-	// )
-	// 	.then((response) => response.json())
-	// 	.then((response) => console.log(response))
-	// 	.catch((err) => console.error(err));
+	const fetchLocation = (event) => {
+		event.preventDefault();
+		const placeOptions = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key':
+					'74dcbb2682msha0be34e87a8094cp16a5dejsn094540852091',
+				'X-RapidAPI-Host':
+					'address-from-to-latitude-longitude.p.rapidapi.com',
+			},
+		};
+
+		fetch(
+			`https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi?address=${location.subregion}%20${location.state}%20${location.country}`,
+			placeOptions,
+		)
+			.then((response) => response.json())
+			.then((response) => {
+				const { latitude, longitude } = response.Results[0];
+				setLocation((prev) => ({ ...prev, latitude, longitude }));
+			})
+			.catch((err) => console.error(err));
+	};
 
 	function handleSubmit() {
 		const options = {
@@ -41,13 +52,12 @@ export default function Home() {
 		};
 
 		fetch(
-			`https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=${latitude}&lon=${longitude}`,
+			`https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily?lat=${location.latitude}&lon=${location.longitude}`,
 			options,
 		)
 			.then((response) => response.json())
 			.then((res) => {
-				console.log(res);
-				setData(res.data);
+				setData(res.data.slice(0, 7));
 				console.log(data);
 			})
 			.catch((err) => console.error('error message: duck you ' + err));
@@ -67,46 +77,70 @@ export default function Home() {
 		return val.replace('T', ' | ');
 	};
 
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setLocation((prev) => ({ ...prev, [name]: value }));
+	};
+
 	return (
-		<div className=" w-full min-h-screen h-full flex justify-center items-center">
+		<div className=" w-full min-h-screen h-full flex justify-center">
 			<div className="flex flex-col space-y-8">
-				<div className="flex justify-center">
-					{/* <input
-						className="py-2 px-3 rounded-md border border-black"
-						type={'text'}
-						value={address}
-						onChange={(e) => setAddress(e.target.value)}
-						placeholder="Enter address ... "
-					/>
-					<input
-						className="py-2 px-3 rounded-md border border-black"
-						type={'text'}
-						value={subregion}
-						onChange={(e) => setSubregion(e.target.value)}
-						placeholder="Enter subregion ... "
-					/>
-					<input
-						className="py-2 px-3 rounded-md border border-black"
-						type={'text'}
-						value={state}
-						onChange={(e) => setState(e.target.value)}
-						placeholder="Enter state ... "
-					/>
-					<input
-						className="py-2 px-3 rounded-md border border-black"
-						type={'text'}
-						value={country}
-						onChange={(e) => setCountry(e.target.value)}
-						placeholder="Enter country ... "
-					/> */}
-					<button onClick={(e) => handleSubmit()}> call fetch</button>
+				<div>
+					<form
+						className="flex flex-col space-y-4 my-6 justify-center"
+						onSubmit={fetchLocation}
+					>
+						<input
+							className="py-2 px-3 rounded-md border border-black"
+							type={'text'}
+							name="subregion"
+							value={location.subregion}
+							onChange={handleChange}
+							placeholder="Enter subregion ... "
+						/>
+						<input
+							className="py-2 px-3 rounded-md border border-black"
+							type={'text'}
+							name="state"
+							value={location.state}
+							onChange={handleChange}
+							placeholder="Enter state ... "
+						/>
+						<input
+							className="py-2 px-3 rounded-md border border-black"
+							type={'text'}
+							name="country"
+							value={location.country}
+							onChange={handleChange}
+							placeholder="Enter country ... "
+						/>
+						<button
+							type="submit"
+							className="rounded-md transition duration-150 mx-auto shadow-xl border-2 border-gray-700/40 text-gray-700/40 hover:border-gray-600/90 hover:text-gray-600/90 w-fit py-2 px-3"
+						>
+							{' '}
+							get Lan & Lon
+						</button>
+					</form>
 				</div>
-				<SimpleContent />
+				<div className="">
+					<button
+						onClick={(e) => {
+							e.preventDefault();
+							handleSubmit();
+						}}
+						className="rounded-md transition duration-150 mx-auto shadow-xl border-2 border-gray-700/40 text-gray-700/40 hover:border-gray-600/90 hover:text-gray-600/90 w-fit py-2 px-3"
+					>
+						{' '}
+						get Data
+					</button>
+				</div>
+
 				<div className="grid grid-cols-5">
 					{data &&
-						data?.map((ele) => {
-							return <WeatherCard elem={ele} />;
-						})}
+						data?.map((ele, ind) => (
+							<WeatherCard key={ind} elem={ele} />
+						))}
 				</div>
 			</div>
 		</div>
