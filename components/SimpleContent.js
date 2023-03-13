71025563fa29928d2a54c6b481bf8bf320caf0a7
k8pai <thek8pai@gmail.com>
2023-03-13 23:50:34 +0530

    initial commit

---
+++ components/SimpleContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SimpleContent = () => {
+	return (
+		<span>
+			here comes the data that's from the request... check console ...
+			shift+ctrl+i
+		</span>
+	);
+};
+
+export default SimpleContent;
